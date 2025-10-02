815896bcd9dd2f92541d4bcd3f0ed58656adccef
Manikanta Akula <maniakula202@gmail.com>
2025-10-02 21:56:39 +0530

    ansible practice

--- 02.count-words.sh
+++ 02.count-words.sh
@@ -2,14 +2,20 @@
 
 inputfile=$1
 
+if [ id -u -ne 0]; then
+    echo "You do not have enough previlage to run this file"
+fi
+
 if [ -d "$inputfile" ]; then
   echo "The given input path is a directory."
   echo "Listing files inside the directory:"
   ls "$inputfile"
+elif [ ! -s "$filename" ]; then
+    echo "The file is Empty"
 elif [ -e "$inputfile" ]; then
-  echo "$(wc -l < $inputfile)"   # Count lines
-  echo "$(wc -w < $inputfile)"   # Count words
-  echo "$(wc -m < $inputfile)"   # Count characters
+  echo "Line count: $(wc -l < $inputfile)"   # Count lines
+  echo "Word count: $(wc -w < $inputfile)"   # Count words
+  echo "Charechter count: $(wc -m < $inputfile)"   # Count characters
 else
   echo "The file does not exist."
 fi
