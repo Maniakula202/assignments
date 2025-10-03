0f8161cfbcbfd6e9e72239d66003e0bc69829add
Manikanta Akula <maniakula202@gmail.com>
2025-10-03 12:06:34 +0530

    ansible practice

--- 03-string-replacement-in-files.sh
+++ 03-string-replacement-in-files.sh
@@ -2,6 +2,10 @@
 
 source_dir=$1
 
+if [ $(id -u) -ne 0 ]; then
+    echo "Error:: Please run this script with root previlege"
+fi
+
 if [ ! -d $source_dir ]; then 
     echo "Error:: Source directory $source_dir does not exists"
     exit 1
@@ -9,4 +13,7 @@ fi
 
 FILES_TO_BE_CHANGE=$(find $source_dir -name "*.conf")
 
-echo "$FILES_TO_BE_CHANGE"
+whenwhile IFS= read -r filepath
+do 
+  echo "Processing: $filepath"
+done <<< $FILES_TO_BE_CHANGE
